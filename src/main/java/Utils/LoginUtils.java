253c02cc4f8fd662d507b52247dfc56ee02b42c4
shadow23244 <githubacc2324@gmail.com>
2025-07-21 23:33:00 +0530

    fix: Use WebDriverManager to automate driver setup

--- src/main/java/Utils/LoginUtils.java
+++ src/main/java/Utils/LoginUtils.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LoginUtils {
 	public static WebDriver driver;
 	public static WebDriverWait wait;
 	
 	public static void initializeDriver() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\evers\\Downloads\\chromedriver-win64\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		driver = new ChromeDriver(options);
